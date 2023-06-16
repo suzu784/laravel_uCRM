44c6a17a0245fce955bc1ca3e6cbd3c7d762a389
suzu784 <s.suzuki784@hotmail.com>
2023-06-17 00:40:05 +0900

    itemsの下準備

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
+use App\Http\Controllers\ItemController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('/items', ItemController::class)
+    ->middleware(['auth', 'verified']);
 
 Route::get(
     '/inertia-test',
@@ -36,10 +29,10 @@ Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('ine
 Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
 
 Route::post('/inertia', [InertiaTestController::class, 'store'])
-->name('inertia.store');
+    ->name('inertia.store');
 
 Route::get('/inertia/create', [InertiaTestController::class, 'create'])
-->name('inertia.create');
+    ->name('inertia.create');
 
 Route::delete('/inertia/{id}', [InertiaTestController::class, 'delete'])->name('inertia.delete');
 
