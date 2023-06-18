f621237628a327af9a9b3e1b9f2875127544d9c3
suzu784 <s.suzuki784@hotmail.com>
2023-06-18 15:03:43 +0900

    商品関連

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ItemSeeder;
 use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            ItemSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
